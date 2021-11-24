53e1a74d4ea4f17c72c3db04602e0b98a50d2e78
Pure <kirill2283371@mail.ru>
2021-11-24 03:10:14 +0300

    Make note images carousel

--- resources/views/notes/notes.blade.php
+++ resources/views/notes/notes.blade.php
@@ -26,13 +26,40 @@
             <div class="mb-3">
                 <div class="fs-5 note-text">{{ $note->text }}</div>
                 @if(!empty(current($note->images))) {{--        Get first object element and check is it empty        --}}
-                    @foreach($note->images as $image)
-                        <div class="d-flex justify-content-center mt-2 imgLoading">
+                    @if($note->images->count() === 1)
+                        <div class="d-flex justify-content-center mt-2 imgLoading imageContainer">
                             <div class="spinner-border" role="status"></div>
-                            <img class="d-none" width="100%"
-                                 src="{{ $image->note_image_path }}">
+                            <img class="d-none h-100 ml-auto mr-auto"
+                                 src="{{ $note->images->first()->note_image_path }}">
                         </div>
-                    @endforeach
+                    @else
+                        <div id="noteImagesCarousel{{ $note->id }}" class="carousel slide" data-bs-interval="false">
+                            <div class="carousel-indicators">
+                                @for($i = 0; $i < $note->images->count(); $i++)
+                                    <button type="button" data-bs-target="#noteImagesCarousel{{ $note->id }}" data-bs-slide-to="{{ $i }}"
+                                            class="{{ $i === 0 ? 'active' : '' }}" aria-current="true" aria-label="Slide {{ $i }}"></button>
+                                @endfor
+                            </div>
+                            <div class="carousel-inner">
+                                @foreach($note->images as $image)
+                                <div class="carousel-item {{ $loop->first ? 'active' : '' }} imgLoading w-100 imageContainer">
+                                    <div class="spinner-border position-absolute bottom-50 end-50" role="status"></div>
+                                    <img src="{{ $image->note_image_path }}" class="d-block h-100 ml-auto mr-auto" alt="Note image" loading="lazy" style="margin-left: auto; margin-right: auto">
+                                </div>
+                                @endforeach
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#noteImagesCarousel{{ $note->id }}"
+                                    data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#noteImagesCarousel{{ $note->id }}"
+                                    data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    @endif
                 @endif
             </div>
             @if(!empty(current($note->user))) {{--        Get first object element and check is it empty        --}}
@@ -52,3 +79,5 @@
     @endforeach
     {{ $notes->onEachSide(1)->links() }}
 </x-app-layout>
+
+{{-- TODO: Адаптировать под светлую тему --}}
