e5100c35ff400d02f14fba6bc8414aaddd3f88df
Pure <kirill2283371@mail.ru>
2022-02-11 22:03:19 +0300

    Fix preventing tab refresh on note editing without changes.

--- public/js/notes/preventTabClosingOnNoteEditing.js
+++ public/js/notes/preventTabClosingOnNoteEditing.js
@@ -1,18 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let isFormsChanged = false
-
-    window.onbeforeunload = (event) => {
+    const windowUnloadListener = (event) => {
         if (isFormsChanged) {
             const dialogText = 'Вы уверены, что хотите выйти? Данные в заметке могут быть не сохранены.';
             event.returnValue = dialogText;
             return dialogText;
         }
-    };
+    }
 
-    document.querySelector('#titleEdit').onchange = document.querySelector('#descriptionEdit').onchange =
-        isFormsChanged = true
+    let isFormsChanged = false
 
-    document.addEventListener('changesSaved', () => {
+    window.addEventListener('beforeunload', windowUnloadListener);
+
+    document.querySelector('#titleEdit').onchange =
+        document.querySelector('#descriptionEdit').onchange =
+        changeFormStatusToTrue
+
+
+    document.addEventListener('changesSaved', changeFormStatusToFalse)
+
+
+    function changeFormStatusToFalse()
+    {
         isFormsChanged = false
-    })
+        window.removeEventListener('beforeunload', windowUnloadListener)
+    }
+
+    function changeFormStatusToTrue()
+    {
+        isFormsChanged = true
+    }
 })
