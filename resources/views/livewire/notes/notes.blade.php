b2774584248fa4d4b0cbdb803590172a2165168c
Pure <kirill2283371@mail.ru>
2022-04-28 18:48:55 +0300

    Add overflow hidden to notes modal.

--- resources/views/livewire/notes/notes.blade.php
+++ resources/views/livewire/notes/notes.blade.php
@@ -59,7 +59,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Вы уверены, что хотите удалить заметку {{ $deletedNote['title'] ?? '' }}?</h5>
+                    <h5 class="modal-title overflow-hidden" id="exampleModalLabel">Вы уверены, что хотите удалить заметку {{ $deletedNote['title'] ?? '' }}?</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-footer">
