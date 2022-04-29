12aec9fc0642d2f26c6807d7244cdcc996e05430
Pure <kirill2283371@mail.ru>
2022-04-29 20:49:05 +0300

    Remove repetition of words from about me. Beautify language selector. Update node js configuration. Run npm production.

--- resources/views/components/language_switcher.blade.php
+++ resources/views/components/language_switcher.blade.php
@@ -1,12 +1,13 @@
-<div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+<div class="d-flex justify-center pt-8 me-1 flex-row">
     @foreach($available_locales as $locale_name => $available_locale)
         @if($available_locale !== $current_locale)
 {{--            <span class="ml-2 mr-2 text-gray-700">{{ $available_locale }}</span>--}}
 {{--        @else--}}
             <form action="{{ route('localization', $available_locale) }}" method="post">
                 @csrf
-                <button type="submit" class="btn btn-info ml-1 underline ml-2 mr-2">
-                    <span>{{ $available_locale }}</span>
+                <button type="submit" class="btn btn-info underline mx-2 mb-1">
+                    <span class="d-none d-lg-block">{{ $available_locale }}</span>
+                    <span class="d-lg-none d-block">{{ $locale_name }}</span>
                 </button>
             </form>
         @endif
