281874c35a6164117fc9dd94480a501b3f8323ef
Pure <kirill2283371@mail.ru>
2022-05-11 00:03:21 +0300

    Fix XSS in notes. Now all < and > are escaped

--- public/js/notes/replaceLinksAndBrInText.js
+++ public/js/notes/replaceLinksAndBrInText.js
@@ -1,27 +1,42 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
     startReplacement()
 })
 
-window.addEventListener('contentChanged', event => {
+window.addEventListener('contentChanged', () => {
     startReplacement()
 });
 
-function startReplacement() {
+function startReplacement()
+{
     document.querySelectorAll('.note-text').forEach((el) => {
-        el.innerHTML = findAndReplaceBr(findAndReplaceLinks(el.textContent))
+        el.innerHTML = findAndReplaceBr(findAndReplaceLinks(escapeTags(el.textContent)))
     })
 }
 
-function findAndReplaceLinks(text) {
+function findAndReplaceLinks(text)
+{
     const regexUrl = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     return text.replaceAll(regexUrl, (url) => {
         return '<a href="' + url + '">' + url + '</a>';
     })
 }
 
-function findAndReplaceBr(text) {
-    const regexUrl = /<br>/gi;
-    return text.replaceAll(regexUrl, () => {
+function findAndReplaceBr(text)
+{
+    console.log(text)
+
+    const regexBr = /&laquo;br \/&raquo;/gi;
+    return text.replaceAll(regexBr, () => {
         return '<br/>';
     })
+}
+
+function escapeTags(text)
+{
+    const regexSmaller =/</gi;
+    const regexBigger = />/gi;
+    const replaceSmallerTo = "&laquo;";
+    const replaceBiggerTo = "&raquo;";
+
+    return text.replaceAll(regexSmaller, replaceSmallerTo).replaceAll(regexBigger, replaceBiggerTo)
 }
