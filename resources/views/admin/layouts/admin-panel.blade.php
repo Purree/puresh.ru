c16e97c72e607e0fac6c768a34e0698b3fa24ea9
Pure <kirill2283371@mail.ru>
2022-06-09 23:07:04 +0300

    Add admin panel navigation.

--- resources/views/admin/layouts/admin-panel.blade.php
+++ resources/views/admin/layouts/admin-panel.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <nav class="navbar navbar-expand">
+    <nav class="navbar navbar-expand pb-0">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav mr-auto bg-dark border-bottom rounded-top">
                 <li class="nav-item">
                     <a class="nav-link {{ Route::is('admin.users') ? 'active' : '' }}" href="{{ route('admin.users') }}">{{ __('Users') }}</a>
                 </li>
