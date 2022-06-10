38e07efefd40a46b3f8a146a0b26dbf51416fda1
Pure <kirill2283371@mail.ru>
2022-06-11 02:50:21 +0300

    Beautify tabs in admin panel.

--- resources/views/admin/layouts/admin-panel.blade.php
+++ resources/views/admin/layouts/admin-panel.blade.php
@@ -1,12 +1,17 @@
 <x-app-layout>
+    <style>
+        .admin-tab-item:not(:last-child) {
+            border-right: 2px solid var(--border_color);
+        }
+    </style>
     <nav class="navbar navbar-expand pb-0">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto bg-dark border-bottom rounded-top">
-                <li class="nav-item">
-                    <a class="nav-link {{ Route::is('admin.users') ? 'active' : '' }}" href="{{ route('admin.users') }}">{{ __('Users') }}</a>
+            <ul class="navbar-nav mr-auto bg-dark border-secondary border rounded-top">
+                <li class="nav-item admin-tab-item">
+                    <a class="nav-link text-light {{ Route::is('admin.users') ? 'active' : '' }}" href="{{ route('admin.users') }}">{{ __('Users') }}</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ Route::is('admin.IPs') ? 'active' : '' }}" href="{{ route('admin.IPs') }}">{{ __('IPs') }}</a>
+                <li class="nav-item admin-tab-item">
+                    <a class="nav-link text-light {{ Route::is('admin.IPs') ? 'active' : '' }}" href="{{ route('admin.IPs') }}">{{ __('IPs') }}</a>
                 </li>
             </ul>
         </div>
